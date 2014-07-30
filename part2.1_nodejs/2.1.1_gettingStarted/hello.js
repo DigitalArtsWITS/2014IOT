8b2941cbf4fa1cd7155af306dfc6e241ee8d48b8
Rouan van der Ende <rouan@8bo.org>
2014-07-30 19:24:13 +0200

    added more html references, cleaned up 2.1

---
+++ part2.1_nodejs/2.1.1_gettingStarted/hello.js
@@ -0,0 +1,14 @@
+//store some numbers in variables called a b and c
+
+var a = 2
+var b = 5
+var c = 999
+
+//runs repeatedly while b is less than or equal to c. 
+//b being 5 is less than c which is 999, so it starts.
+//b is changed to b plus a. so it ads 2 each time.
+
+while (b <= c) {
+    b = b + a
+    console.log("hello " + b)   
+}
